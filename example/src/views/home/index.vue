28cacaa27bd657cce0a44910b947ab455e4feb30
纪泉 <jiquan1997@outlook.com>
2023-10-25 14:40:19 +0800

    delete

--- example/src/views/home/index.vue
+++ example/src/views/home/index.vue
@@ -1,23 +1,21 @@
 <template>
-  <!-- <h1>{{ obj.num }}</h1>
+  <h1>{{ obj.num }}</h1>
   <button @click="change">修改</button>
-  <button @click="reset">重置</button> -->
+  <button @click="reset">重置</button>
 </template>
 
 <script setup lang="ts">
-import { useCookie } from '../../../build/index.js'
-console.log('%c!<----start ---->', 'color:mediumspringgreen')
-console.log(useCookie)
-console.log('%c!<---- end ---->', 'color:mediumspringgreen')
-// let [obj, resetObj] = useResetState({
-//   num: 1,
-// })
-// const change = () => {
-//   obj.num++
-// }
-// const reset = () => {
-//   resetObj()
-// }
+import { useResetState } from '@hooks/index.es.js'
+
+let [obj, resetObj] = useResetState({
+  num: 1,
+})
+const change = () => {
+  obj.num++
+}
+const reset = () => {
+  resetObj()
+}
 </script>
 
 <style scoped lang="less"></style>
